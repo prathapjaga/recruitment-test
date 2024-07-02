2d2364a96c0266e6a04ca0d2d9eb6ba7bbbcbca0
prathap <145013835+prathapjaga@users.noreply.github.com>
2024-07-02 16:01:21 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,26 +1,30 @@
- #include<iostream>
+#include <iostream>
 using namespace std; 
+
 namespace ABC 
 { 
-int data=10; 
-void foo()
- {
- cout<<"in ABC foo :"<<endl; 
-} 
+    int data = 10; 
+    void foo()
+    {
+        cout << "in ABC foo :" << endl; 
+    } 
 } 
 
-namespace   XYZ 
+namespace XYZ 
 {
- float data=2.3;
- void foo() 
-{
- cout<<"in XYZ foo:"<<endl; 
-} 
+    float data = 2.3;
+    void foo() 
+    {
+        cout << "in XYZ foo:" << endl; 
+    } 
 }
- using namespace XYZ; 
+
+using namespace XYZ; 
+
 int main() 
 {
- cout<<data<<endl; 
-ABC::foo(); 
+    cout << XYZ::data << endl;  // Explicitly specify that you want XYZ::data
+    ABC::foo();                 // Explicitly call ABC::foo
 
+    return 0;
 }
